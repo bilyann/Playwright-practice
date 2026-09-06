24375214d2dc2f9da0c625c3e224bf9156fe416f
Anna Bilytskykh <anna2white@gmail.com>
2026-09-06 14:25:46 +0300

    Add GitHub Actions workflow and configure Playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,17 +4,14 @@ export default defineConfig({
   testDir: './tests',
   timeout: 30000,
   use: {
-    // Базова адреса з урахуванням HTTP Basic Auth для доступу до стенду
-    baseURL: 'https://guest:welcome2qauto@qauto.forstudy.space',
+    baseURL: 'https://guest:welcome2qauto@qauto.forstudy.space/',
     trace: 'on-first-retry',
   },
   projects: [
-    // 1. Setup проект, який відповідає за логін і збереження auth state
     {
       name: 'setup',
       testMatch: /auth\.setup\.ts/,
     },
-    // 2. Основний проект, що залежить від setup і виконує тести
     {
       name: 'chromium',
       use: {
